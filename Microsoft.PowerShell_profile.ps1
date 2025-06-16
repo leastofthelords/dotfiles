d93be3785c569a1817f4864382a08833d0542e21
𝑛𝑜𝑑𝑒𝑣 <166487903+nodev7@users.noreply.github.com>
2025-06-16 11:30:14 +0930

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,28 @@
-=== Powershell Profile ==
+===== Powershell Profile ====
 
 winfetch
 scoop update
+
+Set-PsReadlineKeyHandler -Key Tab -Function MenuComplete
+oh-my-posh init pwsh --config "C:\Users\env:username\Dotfiles\greyscale.omp.json" | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+=== Functions ===
 
 function ezai {
 eza --icons
 }
 
-function fzfi {fzf --preview 'bat --style=numbers --color=always --theme="ansi" --line-range=:500 {+}'}
+function fzfi {
+fzf --preview 'bat --style=numbers --color=always --theme="ansi" --line-range=:500 {+}'
+}
 
-Set-PsReadlineKeyHandler -Key Tab -Function MenuComplete
+function cscompile {
+param (
+[string]$Name
+)
+$csc = "$env:WINDIR\Microsoft.NET\Framework\v4.0.30319\csc.exe"
+& $csc "C:\Users\26COSJO\OneDrive - Caritas College\2025\Notepad++\C#\$Name"
+}
 
-oh-my-posh init pwsh --config "C:\Users\Jorda\Dotfiles\greyscale.omp.json" | Invoke-Expression 
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
