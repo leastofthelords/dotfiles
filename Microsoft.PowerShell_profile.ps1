0df88a46e773d0600443d5cc96bc9c157d5cb843
J.C <166487903+leastofthelords@users.noreply.github.com>
2025-12-19 01:15:39 +1030

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,9 @@
 # ===== Powershell Profile ====
 
 fastfetch
-scoop update
 
 Set-PsReadlineKeyHandler -Key Tab -Function MenuComplete
-oh-my-posh init pwsh --config "C:\Users\env:username\Dotfiles\greyscale.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "insertpathhere" | Invoke-Expression
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 # === Functions ===
@@ -17,12 +16,6 @@ function fzfi {
 fzf --preview 'bat --style=numbers --color=always --theme="ansi" --line-range=:500 {+}'
 }
 
-
 # === Import Modules ===
-
-Import-Module cscompile
-Import-Module PoSHFuck
 Import-Module PSScriptAnalyzer
-Import-Module FindOpenFile
-
 
